6ae5d56efeb52d7aaebbfae2f1b896f768c942c9
Petros Kyriakou <9pitops@gmail.com>
2021-09-19 15:17:49 +0300

    fix provider names

--- src/ProvidersManifest.ts
+++ src/ProvidersManifest.ts
@@ -11,19 +11,19 @@ export const ProvidersManifest: Provider[] = [
   {
     name: ProviderName.SLACK,
     type: ProviderType.DIRECT_MESSAGE,
-    packageName: '@messageraft/messageraft-slack',
+    packageName: '@messageraft/slack',
     version: '0.0.1',
   },
   {
     name: ProviderName.TWILIO,
     type: ProviderType.SMS,
-    packageName: '@messageaft/messageraft-twilio',
+    packageName: '@messageraft/twilio',
     version: '0.0.1',
   },
   {
     name: ProviderName.SENDGRID,
     type: ProviderType.EMAIL,
-    packageName: '@messageraft/messageraft-sendgrid',
+    packageName: '@messageraft/sendgrid',
     version: '0.0.1',
   },
 ]
