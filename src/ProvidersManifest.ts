27286778ea60636c5039f34c62288d27439cda4c
Petros Kyriakou <9pitops@gmail.com>
2021-09-19 14:27:36 +0300

    add version to manifest

--- src/ProvidersManifest.ts
+++ src/ProvidersManifest.ts
@@ -1,26 +1,29 @@
-import {ProviderName, ProviderType} from "./interfaces";
-
+import { ProviderName, ProviderType } from './interfaces'
 
 interface Provider {
-    name: ProviderName
-    type: ProviderType
-    packageName: string
+  name: ProviderName
+  type: ProviderType
+  packageName: string
+  version: string
 }
 
 export const ProvidersManifest: Provider[] = [
-    {
-        name: ProviderName.SLACK,
-        type: ProviderType.DIRECT_MESSAGE,
-        packageName: '@messageraft/messageraft-slack',
-    },
-    {
-        name: ProviderName.TWILIO,
-        type: ProviderType.SMS,
-        packageName: '@messageraft/messageraft-twilio',
-    },
-    {
-        name: ProviderName.SENDGRID,
-        type: ProviderType.EMAIL,
-        packageName: '@messageraft/messageraft-sendgrid',
-    },
+  {
+    name: ProviderName.SLACK,
+    type: ProviderType.DIRECT_MESSAGE,
+    packageName: '@messageraft/messageraft-slack',
+    version: '0.0.1',
+  },
+  {
+    name: ProviderName.TWILIO,
+    type: ProviderType.SMS,
+    packageName: '@messageaft/messageraft-twilio',
+    version: '0.0.1',
+  },
+  {
+    name: ProviderName.SENDGRID,
+    type: ProviderType.EMAIL,
+    packageName: '@messageraft/messageraft-sendgrid',
+    version: '0.0.1',
+  },
 ]
